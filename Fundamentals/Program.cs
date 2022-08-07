386aeae0dfa15a99bc5dca0873692c24a903a651
Miroslav Todorov <proudenough@gmail.com>
2022-08-07 18:09:28 +0300

    Add files via upload

---
+++ Fundamentals/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#pragma warning disable CS8604 // Possible null reference argument.
+            int firstNum = int.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+#pragma warning disable CS8604 // Possible null reference argument.
+            int secondNum = int.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+#pragma warning disable CS8604 // Possible null reference argument.
+            int thirdNum = int.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+#pragma warning disable CS8604 // Possible null reference argument.
+            int fourthNum = int.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+
+            int sum = firstNum + secondNum;
+            int devide = sum / thirdNum;
+            int multiply = devide * fourthNum;
+
+            Console.WriteLine(multiply);
+        }
+    }
+}
---
+++ SmallestInteger_Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SmallestInteger(first, second, third));
+        }
+
+        static int SmallestInteger(int one, int two, int three)
+        {
+            if (one < two && one < three)
+            {
+                return one;
+            }
+            else if (two < one && two < three)
+            {
+                return two;
+            }
+            else
+            {
+                return three;
+            }
+        }
+    }
+}

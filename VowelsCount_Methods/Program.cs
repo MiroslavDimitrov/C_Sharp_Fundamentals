a63a653dd62933d7312a651ee8803916ae2a377b
Miroslav Todorov <proudenough@gmail.com>
2022-08-07 18:05:37 +0300

    Add files via upload

---
+++ VowelsCount_Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(CountVowels(input));
+        }
+        /// <summary>
+        /// Method to sum the count of vowels in the input string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        static int CountVowels(string str)
+        {
+            int sum = 0;
+            //loop for the string
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Equals('a') || str[i].Equals('e') || str[i].Equals('i') || str[i].Equals('o') || str[i].Equals('u')
+                    || str[i].Equals('A') || str[i].Equals('E') || str[i].Equals('I') || str[i].Equals('O') || str[i].Equals('U'))
+                {
+                    sum += 1;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ ZIg_Zag_Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input number for iterations of the first loop
+            int input = int.Parse(Console.ReadLine());
+            //first loop for the results with capacity equal to the input number
+            int[] first = new int[input];
+            //second loop with the same purpose as the first one
+            int[] second = new int[input];
+
+            //the loop from where we will take our input data
+            for (int i = 0; i < input; i++)
+            {
+                //take it as an array, and split it
+                int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                //if the number is even we added the data this way so the numbers can be added in a zig-zag way
+                if (i % 2 == 0)
+                {
+                    first[i] = numbers[0];
+                    second[i] = numbers[1];
+                }
+                //odd number
+                else
+                {
+                    first[i] = numbers[1];
+                    second[i] = numbers[0];
+                }
+            }
+
+            //print the results
+            for (int i = 0; i < first.Length; i++)
+            {
+                Console.Write(first[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < second.Length; i++)
+            {
+                Console.Write(second[i] + " ");
+            }
+        }
+    }
+}

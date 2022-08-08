5a4c5765b35ec605faca111ec807056f00c07d55
Miroslav Todorov <proudenough@gmail.com>
2022-08-08 20:17:47 +0300

    Add files via upload

---
+++ Dictionary_A_Miner_Task/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            Dictionary<string, int> results = new Dictionary<string, int>();
+            List<string> materials = new List<string>();
+
+            while (true)
+            {
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                string input = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                if (input == "stop")
+                {
+                    break;
+                }
+
+                if (counter % 2 == 0)
+                {
+#pragma warning disable CS8604 // Possible null reference argument.
+                    if (!materials.Contains(input))
+                    {
+                        materials.Add(input);
+                    }
+#pragma warning restore CS8604 // Possible null reference argument.
+                    counter++;
+                }
+                else if (counter % 2 != 0)
+                {
+                    string key = materials.First();
+#pragma warning disable CS8604 // Possible null reference argument.
+                    int number = int.Parse(input);
+#pragma warning restore CS8604 // Possible null reference argument.
+                    if (!results.ContainsKey(key))
+                    {
+                        results.Add(key, number);
+                        materials.RemoveAt(0);
+                        counter++;
+                    }
+                    else
+                    {
+                        results[key] += number;
+                        materials.RemoveAt(0);
+                        counter++;
+                    }
+                }
+            }
+
+            foreach (var result in results)
+            { 
+                string material = result.Key;
+                int value= result.Value;
+
+                Console.WriteLine($"{material} -> {value}");
+            }
+        }
+    }
+}
---
+++ Dictionary_Count_Char_In_String/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            string input = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+            //dict. for the results
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            //loop for the input
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            for (int i = 0; i < input.Length; i++)
+            {
+                //because we cannot compare char to string
+                string letter = input[i].ToString();
+
+                //if the current character is space --> continue with the next iteration
+                if (letter == " ")
+                {
+                    continue;
+                }
+                else
+                {
+                    //if the letter does not exist in the collectin, add it with value 1
+                    if (!dictionary.ContainsKey(letter))
+                    {
+                        dictionary.Add(letter, 1);
+                    }
+                    else
+                    {
+                        //if exist increase only the value of the current element
+                        dictionary[letter]++;
+                    }
+                }
+            }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+            //print the result
+            foreach (var item in dictionary)
+            {
+                string letter = item.Key;
+                int count = item.Value;
+                Console.Write($"{letter} -> {count}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
